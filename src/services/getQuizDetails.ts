8bc43ef01aaa71c8ef2b6e291d47f75028a97c14
dobereiner.zamudio <dobereiner.zamudiO@gmail.com>
2025-05-26 10:40:27 +0800

    feat: use actual api url

--- src/services/getQuizDetails.ts
+++ src/services/getQuizDetails.ts
@@ -1,14 +1,16 @@
 import { QuizDetails } from "../types";
 import { getQuizDetailsMock } from "../mocks/getQuizDetailsMock";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const getQuizDetails = async (): Promise<QuizDetails> => {
   // Finally add this
-  // const response = await axios.get(
-  //   "https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json"
-  // );
+  const response = await axios.get(
+    "https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json"
+  );
 
-  return getQuizDetailsMock;
+  console.log("response.data!: ", response.data);
+  return response.data;
 };
 
 // Reusable hook
