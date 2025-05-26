2deb799d6e1aa7f26e2ba6c650ca8c81374a3fab
dobereiner.zamudio <dobereiner.zamudiO@gmail.com>
2025-05-27 01:50:17 +0800

    feat: result screen fixed for round 1

--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -2,8 +2,6 @@ import "./styles.css";
 import "../../App.css";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-import { group } from "console";
 
 const pageVariants = {
   initial: { x: "100%", opacity: 0 },
@@ -17,8 +15,8 @@ function Result() {
   const evaluatedResults: string[] = location.state?.data || [];
   const { activity_name } = useParams();
 
-  const filteredAnswers = evaluatedResults.filter((item) =>
-    item.includes("CORRECT")
+  const areQuestionsPerRound: boolean = evaluatedResults.some(
+    (data) => !data.includes("CORRECT")
   );
 
   function groupAnswersByRound(data: any) {
@@ -60,25 +58,52 @@ function Result() {
           <p className="branding">{activity_name?.toUpperCase()}</p>
           <p className="title">RESULTS</p>
           <div className="hide-scroll_view">
-            {Object.entries(groupedAnswers).map(([round, answers], index) => {
-              const answersArray = answers as string[];
-              return (
-                <div key={round}>
-                  <div className="results_item_container_no_flex" key={index}>
-                    <div className="results_evaluated_answer">{round}</div>
-                  </div>
-                  {answersArray.map((answer, index) => {
-                    const label = `Q${questionNumber++}`;
+            {areQuestionsPerRound ? (
+              <>
+                {Object.entries(groupedAnswers).map(
+                  ([round, answers], index) => {
+                    const answersArray = answers as string[];
                     return (
-                      <div className="results_item_container" key={index}>
-                        <div className="results_question_number">{label}</div>
-                        <div className="results_evaluated_answer">{answer}</div>
+                      <div key={round}>
+                        <div
+                          className="results_item_container_no_flex"
+                          key={index}
+                        >
+                          <div className="results_evaluated_answer">
+                            {round}
+                          </div>
+                        </div>
+                        {answersArray.map((answer, index) => {
+                          const label = `Q${questionNumber++}`;
+                          return (
+                            <div className="results_item_container" key={index}>
+                              <div className="results_question_number">
+                                {label}
+                              </div>
+                              <div className="results_evaluated_answer">
+                                {answer}
+                              </div>
+                            </div>
+                          );
+                        })}
                       </div>
                     );
-                  })}
-                </div>
-              );
-            })}
+                  }
+                )}
+              </>
+            ) : (
+              <>
+                {evaluatedResults.map((answer, index) => {
+                  const label = `Q${questionNumber++}`;
+                  return (
+                    <div className="results_item_container" key={index}>
+                      <div className="results_question_number">{label}</div>
+                      <div className="results_evaluated_answer">{answer}</div>
+                    </div>
+                  );
+                })}
+              </>
+            )}
           </div>
           <div className="back" onClick={() => navigate(`/`)}>
             HOME
